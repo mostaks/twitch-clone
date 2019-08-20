44a361e7fda1ded9e30e1c2fdf2c5b2ed8b8b027
Yianni Moustakas <yianni.m13@gmail.com>
2019-08-20 19:22:58 +1000

    Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import Header from './Header';
+
 
 const App = () => {
-    return <div>App</div>
+    return (
+        <div className="ui container">
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" component={StreamCreate} />
+                    <Route path="/streams/edit" component={StreamEdit} />
+                    <Route path="/streams/delete" component={StreamDelete} />
+                    <Route path="/streams/show" component={StreamShow} />
+                </div>
+            </BrowserRouter>
+        </div>
+    )
 };
 
 export default App;
